fa9bb0c3b98420ac6627e2256f26985fdc20c1e9
Tavares <tavares@redes.ufsm.br>
2022-08-12 19:42:09 -0300

    Main

---
+++ src/Domain/Entities/ExampleEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class ExampleEntity : BaseEntity
+    {
+        /// <summary>
+        /// Nome
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Idade
+        /// </summary>
+        public long Age { get; set; }
+    }
+}
